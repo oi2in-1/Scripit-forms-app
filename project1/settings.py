c9ab8da9d85cd7f6ddc91e048fdb39411f280877
script-fourms <88901537+script-fourms@users.noreply.github.com>
2021-08-17 15:13:24 +0100

    Update settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1v&7i-$i@vwe_q=i+f1lxmh5pv#377zln-l%rmhy96wu0m_=_m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-   'project1/static/'
+   'Project1/static'
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
